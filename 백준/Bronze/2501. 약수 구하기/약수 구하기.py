53b43b9626b5b3881b608142abc571418ca2f4e1
kellyn(이가은) <103016025+kellyn0522@users.noreply.github.com>
2025-05-17 22:56:13 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+# 2501번 - 약수 구하기
+n, k = list(map(int, input().split()))
+arr = []
+
+for i in range(1, n+1):
+    if n % i == 0:
+        arr.append(i)
+
+if len(arr) < k:
+    print("0")
+else: 
+    print(arr[k-1])
